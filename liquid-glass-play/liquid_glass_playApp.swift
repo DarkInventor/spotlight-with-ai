b5c5d0299a3a7b845cc9c5c1833c1a7b4f3a7fc3
DarkInventor <ktmehta25@gmail.com>
2025-06-27 04:55:39 -0400

    Initial Commit

---
+++ liquid-glass-play/liquid_glass_playApp.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_glass_playApp.swift
+//  liquid-glass-play
+//
+//  Created by Kathan Mehta on 2025-06-27.
+//
+
+import SwiftUI
+
+@main
+struct liquid_glass_playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ liquid-glass-playTests/liquid_glass_playTests.swift
@@ -0,0 +1,16 @@
+//
+//  liquid_glass_playTests.swift
+//  liquid-glass-playTests
+//
+//  Created by Kathan Mehta on 2025-06-27.
+//
+
+import Testing
+
+struct liquid_glass_playTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
